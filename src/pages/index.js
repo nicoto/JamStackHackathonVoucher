3993896a8e69ce9b2a6fda34dd03ef3a52726eed
nicoto <toulousenicholas@gmail.com>
2018-08-29 20:46:07 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () =>
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hi people, My name is Nick and I am a design technologist living&nbsp;in San Francisco and I would like to begin using microservices and APIs to improve my websites.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+
+export default IndexPage
