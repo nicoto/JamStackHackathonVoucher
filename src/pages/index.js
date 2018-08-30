d2313731ae7aa40388aa7c7757f25e09dc8b7c51
Nicholas Toulouse <6207181+nicoto@users.noreply.github.com>
2018-08-29 21:04:18 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () =>
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Hi people, My name is Nick and I am a design technologist living&nbsp;in San Francisco and I would like to begin using microservices and APIs to improve my websites.</p>
+    <p>Hi people, My name is Nick and I am a design technologist and I would like to begin using microservices and APIs to make a website even better.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 
